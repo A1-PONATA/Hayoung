b3b144f960a43dde0ea96c5ada8d38d63f84c3d7
hayoung.is.here <hayoung.is.here@gmail.com>
2019-12-12 15:39:40 +0900

    change w,h in webcam input

--- merge_ver/webcam.py
+++ merge_ver/webcam.py
@@ -7,8 +7,8 @@ def get_frame():
     # VideoCapture :: If it does not work, try setting to '1' instead of '0'
     cam = cv2.VideoCapture(1)
     cam.set(cv2.CAP_PROP_FPS, 10)
-    cam.set(3, 320)  # 3 : width
-    cam.set(4, 320)  # 4 : height
+    cam.set(3, 180)  # 3 : width
+    cam.set(4, 240)  # 4 : height
     # time.sleep(0.1)
     ret, frame = cam.read()
     t1 = time.time()
